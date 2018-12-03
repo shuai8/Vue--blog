b5add7bdf2c8255612bc5acc6f396a475beb1b83
stan <1427399030@qq.com>
2018-12-03 18:46:12 +0800

    帅仔

---
+++ src/components/front/component/MyFooter.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <a class="iconfont icon-github" href="https://github.com/shuai8"></a>
+        <p>粤ICP备17036548号</p>
+        <p>Copyright @ 2017 Powered by Vue. Designed by me | <router-link :to="{name: 'login'}" tag="span" class="login">站长登录</router-link></p>
+    </div>
+</template>
+
+<script>
+export default {}
+
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+    .iconfont {
+       font-size: 1.875rem;
+       color: #fff;
+       margin-right: 0.625rem;
+    }
+    .icon-github:hover {
+             color: royalblue;
+    }
+    .icon-cion15 {
+        font-size: 1.625rem;
+    }
+    .icon-cion15:hover {
+        color: deepskyblue;
+    }
+    .icon-iconsf:hover {
+        color: green;
+    }
+    div {
+        text-align: center;
+        padding-top: 0.625rem;
+        line-height: 2.5rem;
+        width: 100%;
+        background: rgba(55, 57, 65, 0.5);
+    }
+    p {
+        text-align: center;
+        color: #ffffff;
+        font-size: 1rem;
+    }
+    span.login {
+        cursor: pointer;
+        color: #fff;
+    }
+</style>
